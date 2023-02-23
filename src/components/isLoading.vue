00fc426bf1f9b7cfbae783bcbba1c1961b3ab976
freddykaberuka <freddykaberuka2009@gmail.com>
2023-02-23 02:14:55 +0200

    add a pagination

---
+++ src/components/isLoading.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div class="mt-5 flex justify-center items-center">
+    <h2 class="text-gray-300 animate-pulse">
+      {{ store.loadingMessage }} . . .
+    </h2>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,44 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import Movies from "../components/MovieList.vue";
 import { moviesStore } from "../stores/movies";
+import isLoading from "../components/isLoading.vue";
+
 const store = moviesStore();
+const scrollComponent = ref(null);
+const keyword = ref(
+  localStorage.getItem("keyword")
+    ? localStorage.getItem("keyword")
+    : "anaconda"
+);
+let totalPage = 0;
+setTimeout(() => {
+  totalPage = Math.ceil(store.totalResults / 10);
+}, 1000);
+
+const handleScroll = (e) => {
+  const element = scrollComponent.value;
+  if (element) {
+    if (element.getBoundingClientRect().bottom < window.innerHeight) {
+      store.page++;
+      if (store.page <= totalPage) {
+        store.nextPage(store.page);
+      }
+    }
+  }
+};
 
 onMounted(() => {
-  store.getAllMovies();
+  store.getAllMovies(keyword.value);
 });
 console.log(store.movies, "12");
 </script>
 
 <template>
   <main>
-    <Movies :movies="store.movies" />
+     <article ref="scrollComponent">
+      <Movies :movies="store.movies" />
+    </article>
+    <IsLoading v-if="store.isLoading" />
   </main>
 </template>
