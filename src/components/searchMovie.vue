449e49dd40d4dfed98bda6d67ddf9a6ecd87d088
freddykaberuka <freddykaberuka2009@gmail.com>
2023-02-23 12:33:31 +0200

    add search functionality

--- src/components/searchMovie.vue
+++ src/components/searchMovie.vue
@@ -1,13 +1,42 @@
 <script setup>
+import { ref } from "vue";
 import SearchIcon from "./icons/SearchIcon.vue";
+import { moviesStore } from "../stores/movies";
+const store = moviesStore();
+
+const keyword = ref(localStorage.getItem("keyword") ?? "");
+const isRecent = ref(localStorage.getItem("isRecent") == "true" ? true : false);
+const recents = ref(
+  localStorage.getItem("recents")
+    ? JSON.parse(localStorage.getItem("recents"))
+    : []
+);
+const handleSearch = () => {
+  localStorage.setItem("keyword", keyword.value);
+  localStorage.setItem("isRecent", true);
+
+  isRecent.value = true;
+  let str = keyword.value.toLowerCase();
+  str = str.trim();
+  if (keyword.value) {
+    const cek = recents.value.filter((item) => item == str);
+    if (cek.length < 1) {
+      recents.value.push(str);
+      localStorage.setItem("recents", JSON.stringify(recents.value));
+    }
+    store.getAllMovies(str);
+  }
+};
 </script>
 <template lang="">
-  <form>
+  <form @submit.prevent="handleSearch">
     <div class="relative text-gray-300">
       <input
         type="text"
         placeholder="search movie here..."
         class="w-full px-14 py-3 rounded-md bg-stone-700 border border-slate-400 focus:ring focus:ring-slate-400 focus:ring-slate-400/50 md:text-lg lg:w-6/6 lg:text-sm lg:px-10"
+        v-model="keyword"
+        @keydown.enter.prevent="handleSearch"
       />
       <SearchIcon />
     </div>
